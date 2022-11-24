50965c51769d4a480e30e4a5a56cc7fef4675e58
Jim Hurd <jimh@datagrove.com>
2022-11-24 18:03:57 -0500

    a

---
+++ packages/shop/.temp.1669330901067.tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	mode: 'jit',
+	darkMode: 'class',
+	theme: {
+		extend: {},
+	},
+	plugins: [
+		require('@tailwindcss/typography'),
+		require("daisyui"),
+	],
+}
